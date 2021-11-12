824acde958514d4dfc3c135cc282e0655f0bc795
Daniel M <daniel.martensson89@outlook.com>
2021-11-12 11:57:03 +0100

    Added apiUrl, Todo CORS on API

--- TodoWeb/ClientApp/src/app/todo/todo.component.ts
+++ TodoWeb/ClientApp/src/app/todo/todo.component.ts
@@ -1,15 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ApiPaths } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+    selector: 'app-todo',
+    templateUrl: './todo.component.html',
+    styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+    public message: string = '';
 
-  ngOnInit(): void {
-  }
+    constructor(http: HttpClient) {
+        http.get<string>(ApiPaths.ApiBaseUrl).subscribe(result => {
+            this.message = result;
+        }, error => console.error(error));
+    }
+    ngOnInit(): void {
+    }
+}
 
+
+interface WeatherForecast {
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
