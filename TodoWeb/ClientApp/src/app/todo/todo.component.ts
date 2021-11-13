cb6f9745356284816c6b62c0b904d862d58e105b
Daniel Mårtensson <daniel.martensson89@outlook.com>
2021-11-13 23:41:14 +0100

    Add new Todo

--- TodoWeb/ClientApp/src/app/todo/todo.component.ts
+++ TodoWeb/ClientApp/src/app/todo/todo.component.ts
@@ -1,5 +1,4 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApiPaths } from 'src/environments/environment';
 
@@ -23,20 +22,32 @@ export class TodoComponent implements OnInit {
     }
 
     saveTodo(todo: Todo): void {
-        this.httpClient.put<any>(ApiPaths.ApiBaseUrl + 'todoitems/' + todo.id, {
+        const savingTodo: Todo = {
             id: todo.id,
             name: todo.name,
             isComplete: todo.isComplete
-        }).subscribe(result => {
-        }, error => console.error(error));
+        }
+
+        if (savingTodo.id == 0) {
+            this.httpClient.post<Todo>(ApiPaths.ApiBaseUrl + 'todoitems', savingTodo).subscribe(result => {
+            }, error => console.error(error));
+        }
+        else{
+            this.httpClient.put<any>(ApiPaths.ApiBaseUrl + 'todoitems/' + todo.id, savingTodo).subscribe(result => {
+            }, error => console.error(error));
+        }
     }
 
     deleteTodo(todo: Todo): void {
         this.httpClient.delete<Todo>(ApiPaths.ApiBaseUrl + 'todoitems/' + todo.id).subscribe(result => {
             this.todos.forEach((element, index) => {
-                if(element.id === result.id) this.todos.splice(index, 1);
+                if (element.id === result.id) this.todos.splice(index, 1);
             })
         }, error => console.error(error));
+    }
+
+    createTodo(): void {
+        this.todos.push({ id: 0, name: 'new todo', isComplete: false });
     }
 }
 
